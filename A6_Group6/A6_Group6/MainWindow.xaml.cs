106aa5c13316d73837c5df40ec61bcdf26db18a1
hsahluwalia9 <harsimar.ahluwalia@gmail.com>
2016-10-14 16:10:43 -0700

    Add files via upload

---
+++ A6_Group6/A6_Group6/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace A6_Group6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        VM vm;
+        public MainWindow()
+        {
+            InitializeComponent();
+            vm = new VM();
+            DataContext = vm;
+        }
+
+        private void BtnScissor_Click(object sender, RoutedEventArgs e)
+        {
+            
+           
+            
+        }
+
+        private void BtnPaper_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void BtnRock_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void BtnBegin_Click(object sender, RoutedEventArgs e)
+        {
+          
+        }
+    }
+}
---
+++ A6_Group6/A6_Group6/VM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace A6_Group6
+{
+    class VM : INotifyPropertyChanged
+    {
+        public int randNum;
+
+        public void Random()
+        {
+            Random r = new Random();
+            randNum = r.Next(1, 4);
+        }
+
+        
+        
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName]string caller = null)
+        {
+
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(caller));
+            }
+        }
+        #endregion
+    }
+}
